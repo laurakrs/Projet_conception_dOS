dafb30404bf183deba99ed40b635a74278db34c2
laurakrs <laurakrs>
2024-11-16 16:10:04 +0100

    Add linked list

--- src_de_base/process.h
+++ src_de_base/process.h
@@ -1,6 +1,7 @@
 #include <inttypes.h>
 #include <stdio.h>
-#include <tinyalloc.h>       
+#include <tinyalloc.h>   
+
 
 
 #define N_REG 5
@@ -8,17 +9,20 @@
 #define SIZE_S 512
 
 #define N_PROC 8
+
+#define MAX_LENGTH_NOM 50
+
 // avant = 2 processus
 
 // etat du processus : dans ce premier exemple, il n’y a que 2 états possibles : élu ou activable, vous
 // pouvez toutefois déjà définir proprement les états comme une énumération de constantes
 enum ETAT {ELU, ACTIVABLE};
 
-typedef struct  {
+typedef struct PROCESS {
     // le pid du processus, sous la forme d’un entier (signe -1 en cas derreur)
     uint32_t pid;
     // un chaine de caractères avec une taille maximum fixée ;
-    char nom[50];
+    char nom[MAX_LENGTH_NOM];
     // dans ce premier exemple, il n’y a que 2 états possibles : élu ou activable, 
     enum ETAT etat;
 
@@ -32,12 +36,27 @@ typedef struct  {
     une constante, par exemple 512.
     */
    int stack[SIZE_S];
+
+   // champ suiv défini comme un pointeur vers une structure de processus : 
+   // ce champ va permettre de chainer les processus les uns aux autres ;
+   struct PROCESS *suiv;
+
 } PROCESS;
 
 void idle(void);
 
 
 void proc1(void);
+
+void proc2(void);
+
+void proc3(void);
+
+void proc4(void);
+
+void proc5(void);
+
+void proc6(void);
 
 // assembly
 // Elle prend 2 paramètres de types pointeurs sur des entiers : il s’agit en fait des adresses des zones de sauvegarde des registres des contextes de l’ancien processus et du nouveau.
@@ -53,7 +72,29 @@ int32_t cree_processus(void (*code)(void), char *nom);
 
 
 // table globale
+// table globale
+// extern PROCESS table_proc[N_PROC];
 extern PROCESS* table_proc[N_PROC];
+
+/* définir une liste des activables : il est sûrement plus efficace de conserver deux pointeurs, un sur
+la tête et un autre sur la queue de la liste, pour garantir l’insertion en queue à coût constant.*/
+
+
+// LIST
+typedef struct {
+    PROCESS *tete;
+    PROCESS *queue;
+} ListActivables;
+
+void init_list(ListActivables *list);
+
+
+// Fonction d'insertion en queue
+void inserer_queue(ListActivables *list, PROCESS *process);
+
+// Fonction d'extraction de la tete
+PROCESS *extraire_tete(ListActivables *list);
+
 
 
 
